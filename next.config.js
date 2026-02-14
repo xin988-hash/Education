0e2c371bfdc5f88af167d654c1918a1df177226c
xin988-hash <rongxinchen988@gmail.com>
2026-02-14 14:38:30 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
-const withPWA = require('next-pwa')({
-  dest: 'public',
-  disable: process.env.NODE_ENV === 'development',
-})
-module.exports = withPWA({
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   reactStrictMode: true,
-})
+}
+
+module.exports = nextConfig
